5d71ded9a8c5fa2dc0ec35a38884b7d49e4a47a0
Ling Ding <azusakaworu@gmail.com>
2019-02-18 15:39:46 -0500

    update

--- day2/购物车列表编码与Bootstrap使用/js/main.js
+++ day2/购物车列表编码与Bootstrap使用/js/main.js
@@ -1,24 +1,35 @@
-//js
+//js 购物车小练习（1）
 
 /*
-购物车小练习（1）
+专门用来发送Ajax的函数：
+                       created:function(){}
+                       用this调用在methods里写好的fetch data 的函数
 
+专门写函数的地方 ： 
+                methods:{} 
+                
 */
 
 let vm = new Vue({
 
 	el:'#app',
+  filters:{//可以有好多自定义过滤器
+    toFixed(input,param1){//这里的this是window
+      //参数input是管道符之前的东西 param1是toFixed中传递的参数
+         return "$"+input.toFixed(param1);
+        //return 100;
+    }
+
+  },
 	data:{
 
 		products:[]
           },
 
-    created:function(){
-
-      this.getData();//调用函数 this是vm
-
-    },
+    created:function(){this.getData();//调用函数 this是vm
+                       },
     methods:{
+      remove(p){this.products = this.products.filter(item=> item !==p);},
       getData:function(){//把这一套code封装到一个函数里
           axios.get("./carts.json").then(
                res=>{console.log(res);//res是自带的对象 定义形参给实参
@@ -26,5 +37,9 @@ let vm = new Vue({
               err=>{console.log(err);});
 
       }
+    
+    
+
+
     }
 });
